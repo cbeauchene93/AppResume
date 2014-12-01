3bec4f413048b7dbf0c08f8b7f948b4b65313232
Christopher Beauchene <Chris@Christophers-MacBook-Pro.local>
2014-12-01 05:08:54 -0600

    Initial Commit

---
+++ AppResume/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppResume
+//
+//  Created by Christopher Beauchene on 12/1/14.
+//  Copyright (c) 2014 ChrisBeauchene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppResume/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppResume
+//
+//  Created by Christopher Beauchene on 12/1/14.
+//  Copyright (c) 2014 ChrisBeauchene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
